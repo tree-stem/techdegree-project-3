868c7c20edeb785210a4276c266169344111bd4f
Tristan Ringer <rin9er@gmail.com>
2024-11-13 13:31:00 -0700

    Other job role text box updated

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,2 +1,19 @@
 // focuses on the first text field once page loads
 document.getElementById('name').focus();
+
+// created variables to hide 'Other job role?' text box unless selected using an addEventListener
+const otherTextBox = document.getElementById('other-job-role');
+const jobs = document.getElementById('title');
+
+otherTextBox.style.display = 'none';
+
+jobs.addEventListener('change', () => {
+    
+    if (jobs.value === 'other') {
+        otherTextBox.style.display = 'block';
+        otherTextBox.focus();
+    } else {
+        otherTextBox.style.display = 'none';
+    }
+});
+
